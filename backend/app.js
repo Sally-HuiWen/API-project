34f79cabf371fa2d8def4b6be30205f3ba3cd6c0
Hui Wen <lovelyelsa@macbook-air-8.lan>
2024-03-27 16:16:56 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,13 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 
-const routes = require('./routes');
-
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+
+const routes = require('./routes');
+
+const { ValidationError } = require('sequelize');
+
 
 const app = express();
 app.use(morgan('dev'));
@@ -21,28 +24,63 @@ app.use(express.json());
 if (!isProduction) {
     // enable cors only in development
     app.use(cors());
+}
+  
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
+  
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+app.use(routes); // Connect all the routes
+
+// Phase 2/part1: Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// Phase 2/part2: Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
   }
-  
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
-  
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
-
-  app.use(routes); // Connect all the routes
+  next(err);
+});
 
 
+// Phase 2/part3: Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
-  module.exports = app;
+
+module.exports = app;
