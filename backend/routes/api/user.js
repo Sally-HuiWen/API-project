c24db3ee7e73a762907a6543c8156a493bea3eb9
Hui Wen <lovelyelsa@macbook-air-8.lan>
2024-03-29 08:54:29 -0700

    add-firstName-lastName-tp-table-Users

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -10,6 +10,12 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const validateSignup = [
+  check('firstName')
+  .exists({ checkFalsy: true })
+  .withMessage('Please provide a valid firstName.'),
+  check('lastName')
+  .exists({ checkFalsy: true })
+  .withMessage('Please provide a valid lastName.'),
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
@@ -34,12 +40,14 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { firstName, lastName, email, password, username } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ firstName, lastName, email, username, hashedPassword });
   
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
       };
